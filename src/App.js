72d929f0a5d5d587d2936f9aac67c48bbd275102
chandramouli3235 <tarigopulachandramouli1818@gmail.com>
2024-04-08 19:21:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TopStrip from './Components/TopStrip';
+import Home from './Components/Home';
+import Footer from './Components/Footer';
+
+const App = () => (
+    <div>
+        <TopStrip />
+        <Home />
+        <Footer />
+    </div>
+)
+
+export default App;
